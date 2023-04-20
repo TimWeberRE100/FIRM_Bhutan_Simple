c268c3961fdaeb589fa72f7979a6ed648a7c9f0b
TimWeberRE100 <Timothy.Weber@anu.edu.au>
2023-04-20 17:13:19 +1000

    Flat demand curve scenarios

--- Optimisation.py
+++ Optimisation.py
@@ -9,11 +9,11 @@ import datetime as dt
 import csv
 
 parser = ArgumentParser()
-parser.add_argument('-i', default=100, type=int, required=False, help='maxiter=4000, 400')
-parser.add_argument('-p', default=2, type=int, required=False, help='popsize=2, 10')
+parser.add_argument('-i', default=1500, type=int, required=False, help='maxiter=4000, 400')
+parser.add_argument('-p', default=5, type=int, required=False, help='popsize=2, 10')
 parser.add_argument('-m', default=0.5, type=float, required=False, help='mutation=0.5')
 parser.add_argument('-r', default=0.3, type=float, required=False, help='recombination=0.3')
-parser.add_argument('-e', default=6, type=int, required=False, help='per-capita electricity = 3, 6, 20 MWh/year')
+parser.add_argument('-e', default=6, type=int, required=False, help='per-capita electricity = 3, 6, 20 MWh/year; prefix 2 for flat curves')
 parser.add_argument('-n', default='Super', type=str, required=False, help='Super, CH, TH...')
 parser.add_argument('-s', default='existing', type=str, required=False, help='all, construction, existing')
 args = parser.parse_args()
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 python3 Optimisation.py -e 3 -s existing
 python3 Optimisation.py -e 6 -s existing
 python3 Optimisation.py -e 20 -s existing
@@ -7,3 +8,12 @@ python3 Optimisation.py -e 20 -s construction
 python3 Optimisation.py -e 3 -s all
 python3 Optimisation.py -e 6 -s all
 python3 Optimisation.py -e 20 -s all
+python3 Optimisation.py -e 23 -s existing
+python3 Optimisation.py -e 26 -s existing
+python3 Optimisation.py -e 220 -s existing
+python3 Optimisation.py -e 23 -s construction
+python3 Optimisation.py -e 26 -s construction
+python3 Optimisation.py -e 220 -s construction
+python3 Optimisation.py -e 23 -s all
+python3 Optimisation.py -e 26 -s all
+python3 Optimisation.py -e 220 -s all
