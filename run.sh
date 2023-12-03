500698354f8e6a36a8fb456cca742c27013e204e
u5144821 <anna.nadolny@anu.edu.au>
2023-12-03 19:24:05 +1100

    Fix numbers in assets

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-python3 Optimisation.py -e 3 -s existing -y no_import -p 10 -i 1000
+python3 Optimisation.py -e 76 -s existing -y no_import -p 10 -i 1000 running
 python3 Optimisation.py -e 6 -s existing -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 10 -s existing -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 15 -s existing -y no_import -p 10 -i 1000
-python3 Optimisation.py -e 3 -s construction -y no_import -p 10 -i 1000
+python3 Optimisation.py -e 76 -s construction -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 6 -s construction -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 10 -s construction -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 15 -s construction -y no_import -p 10 -i 1000
-python3 Optimisation.py -e 3 -s all -y no_import -p 10 -i 1000
+python3 Optimisation.py -e 76 -s all -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 6 -s all -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 10 -s all -y no_import -p 10 -i 1000
 python3 Optimisation.py -e 15 -s all -y no_import -p 10 -i 1000
